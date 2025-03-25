ac51d5594ff6828b797c239a88f5a0b0e5720536
Zarina-Gaffarova <zarinagaffarova878@gmail.com>
2025-03-25 13:20:06 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -180,3 +180,59 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+// Объявляем переменную-массив intensiveImg и сохраняем в нее все элементы на странице с классом intensive__img  
+const intensiveImg = document.querySelectorAll('.intensive__img');
+
+// Пройдемся по каждому элементу массива intensiveImg, с помощью цикла forEach.  Внутри функции 2 переменные: 
+// item - текущее изображение, а index — его номер в массиве, начиная с 0
+intensiveImg.forEach((item, index) => {
+
+// Объявляем переменную intensiveText и сохраняем в нее все элементы с классом intensive__description, которые связаны с описаниями для изображений
+    const intensiveText = document.querySelectorAll('.intensive__description');
+
+// Когда курсор наводится на изображение (событие mouseenter), срабатывает обработчик события mouseenter:
+    item.addEventListener('mouseenter', () => {
+	// Делаем изображение полупрозрачным
+      item.style.opacity = 0.5;
+	// И удаляем атрибут hidden и текст становится видимым
+      intensiveText[index].removeAttribute('hidden');
+    });
+
+// Когда курсор убираем с изображения (событие mouseleave), срабатывает обработчик события mouseleave:
+    item.addEventListener('mouseleave', () => {
+	// Изображение делаем непрозрачным
+      item.style.opacity = 1;
+	// И добавляем атрибут hidden и текст становится видимым
+      intensiveText[index].setAttribute('hidden', true);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    // * 1. Начало.
+    // * 2. Получаем все элементы изображений с описанием.
+    // * 3. Для каждого изображения (проверяем есть ли такие изображения):
+    // * 3.1. Добавляем обработчик наведения курсора на изображение:
+    // * 3.1.1. Да:
+    // * 3.1.1.1. показываем текст при наведении.
+    // * 3.1.2. Нет: продолжаем.
+    // * 3.2. Добавляем обработчик курсор уходит с изображения:
+    // * 3.3.1. Да:
+    // * 3.3.1.1. Скрываем элемент с описанием.
+    // * 3.3.2. Нет: продолжаем.
+    // * 4. Конец
+
+    const intensiveImg = document.querySelectorAll('.image-js');
+   
+    intensiveImg.forEach((item, index) => {
+        const intensiveText = document.querySelectorAll('.description-js');
+        item.addEventListener('mouseenter', () => {
+            item.style.opacity = 0.5;
+            intensiveText[index].removeAttribute('hidden');
+        });
+        item.addEventListener('mouseleave', () => {
+            item.style.opacity = 1;
+            intensiveText[index].setAttribute('hidden', true);
+        });
+    });
+});
+    
